8a676a7a8d103eacb6354fdf6c7a7d4fbc6a3cf4
unknown <abanoob.meseha@gmail.com>
2025-03-08 19:59:03 +0200

    fixed: Profile image not showing bug

--- src/components/Hero/ImageSection.tsx
+++ src/components/Hero/ImageSection.tsx
@@ -8,6 +8,7 @@ const ImageSection = () => {
         className="rounded-full drop-shadow-2xl border md:w-[300px] md:h-[300px] w-[200px] h-[200px] bg-indigo-900 md:-mt-4 animate-glow"
         width={200}
         height={200}
+        unoptimized
       />
   );
 };
