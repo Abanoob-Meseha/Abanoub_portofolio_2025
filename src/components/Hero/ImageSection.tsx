c826824821089f6b2ddda02a372338a897fce060
unknown <abanoob.meseha@gmail.com>
2025-03-08 20:01:25 +0200

    fixed: Profile image not showing bugs

--- src/components/Hero/ImageSection.tsx
+++ src/components/Hero/ImageSection.tsx
@@ -8,7 +8,6 @@ const ImageSection = () => {
         className="rounded-full drop-shadow-2xl border md:w-[300px] md:h-[300px] w-[200px] h-[200px] bg-indigo-900 md:-mt-4 animate-glow"
         width={200}
         height={200}
-        unoptimized
       />
   );
 };
