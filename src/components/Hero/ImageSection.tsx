09376a0831c64ddbe202fbe0c0d1404c7d7bed6b
unknown <abanoob.meseha@gmail.com>
2025-03-09 19:23:54 +0200

    try to fix profile image on vercel

--- src/components/Hero/ImageSection.tsx
+++ src/components/Hero/ImageSection.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const ImageSection = () => {
   return (
       <Image
-        src={"/profile_2025.png"}
+        src="/profile_2025.png"
         alt="Abanoub Profile Image"
         className="rounded-full drop-shadow-2xl border md:w-[300px] md:h-[300px] w-[200px] h-[200px] bg-indigo-900 md:-mt-4 animate-glow"
         width={200}
